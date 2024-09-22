b48620ccc7091ac13fe250176e9d4bb1e0c9143b
Jakub Golc <jakub.golcgdy@gmail.com>
2024-09-22 13:51:03 +0200

    Add plot_microstate and reorder_microstates functions

--- clustering/utilities/functions.py
+++ clustering/utilities/functions.py
@@ -3,6 +3,8 @@ import pickle
 import re
 import os
 import shutil
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 from math import log2
 from scipy.interpolate import griddata
@@ -11,6 +13,49 @@ from datetime import datetime
 FILE_NAME =  os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     "cap2.csv")
+
+MICROSTATE_A = np.array([
+    -30,   -30,
+    0,   -30, -30,
+    30, 0, -30 ,-30, -30,
+    30,   30, 30,   30,  0,  -30, -30,  -30,  -30,
+    30, 30, 30,  0,  -30,
+    30,  30, 0,
+    30, 0, 
+    30])
+MICROSTATE_B = np.array([
+    -30,   -30,
+    -30,   -30, 0,
+    -30, -30, -30 , 0, 30,
+    -30, -30, -30,   -30,  0,  30, 30,  30,  30,
+    -30, 0, 30,  30,  30,
+    0,  30, 30,
+    0, 30, 
+    30])
+MICROSTATE_C = np.array([
+    -30,   -30,
+    -30,   -30, -30,
+    -30, -30, -30 , -30, -30,
+    -30, -30, -30, -30, -30, -30, -30, -30,  -30,
+    30, 30, 30,  30,  30,
+    30, 30, 30,
+    30, 30, 
+    30])
+MICROSTATE_D = np.array([
+    -30,   -30,
+    -30,   -30, -30,
+    30, -30, -30 , -30, -30,
+    30, 30, -30, -30, -30, -30, -30, -30,  30,
+    30, -30, -30,  -30,  30,
+    30, -30, 30,
+    30, 30, 
+    30])
+REFERENCE_MICROSTATES = np.array([
+    MICROSTATE_A,
+    MICROSTATE_B,
+    MICROSTATE_C,
+    MICROSTATE_D
+])
 
 def eeg_to_map(data, n_grid=64):
     """
@@ -195,3 +240,41 @@ def extract_date(filename):
     parts = filename.split('_')
     date_str = '_'.join(parts[:3])
     return datetime.strptime(date_str, '%d_%m_%y')
+
+def plot_microstate(microstate):
+    cm = mpl.colormaps['seismic']
+    plt.imshow(eeg_to_map(microstate), cmap=cm, origin='lower')
+    plt.axis('off')
+    plt.show()
+    plt.clf()
+
+def reorder_microstates(microstates: np.array, reference_microstates = None):
+    reordered_microstates = np.ones(microstates.shape)
+    # Get correlations between microstates and it's references
+    microstates_normalized = microstates - np.mean(microstates, axis=1, keepdims=True) 
+    microstates_normalized /= np.std(microstates, axis=1, keepdims=True)
+    if not reference_microstates:
+        reference_microstates = REFERENCE_MICROSTATES
+    reference_microstates_normalized = reference_microstates - np.mean(reference_microstates, axis=1, keepdims=True)
+    reference_microstates_normalized /= np.std(reference_microstates, axis=1, keepdims=True) 
+    corr = np.dot(
+        microstates_normalized, 
+        reference_microstates_normalized.T) / len(microstates[0])
+    corr = corr ** 2
+    print(corr)
+    print()
+    # Pick the highest correlation and leave other for next iteration
+    old_positions = [1,2,3,4]
+    new_positions = []
+    for _ in microstates:
+        max_corr = np.max(corr, axis=1)
+        max_positions = np.argmax(corr, axis=1)
+        # Get position of microstate with maximal correlation value
+        current_position = np.argmax(max_corr)
+        new_position = max_positions[np.argmax(max_corr)]
+        # new_positions[current_position] = new_position
+        reordered_microstates[new_position] = microstates[current_position]
+        # Remove this microstate correlation from next iterations
+        corr[:, new_position] = -1
+    print(reordered_microstates)
+    return reordered_microstates
