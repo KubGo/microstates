453653bf2b649f198d446e5ac7e007dfed244d4f
Jakub Golc <jakub.golcgdy@gmail.com>
2024-11-21 18:49:41 +0100

    Add home page

--- gui/src/app_layout.py
+++ gui/src/app_layout.py
@@ -1,13 +1,14 @@
 from flet import (
     Page,
     Row,
-    Text,
     icons,
     colors,
     IconButton,
+    alignment
 )
 
 from sidebar import SideBar
+from home_page import HomePage
 
 class AppLayout(Row):
     def __init__(
@@ -27,13 +28,24 @@ class AppLayout(Row):
             selected_icon=icons.ARROW_CIRCLE_RIGHT,
             on_click=self.toggle_nav_rail)
         self.sidebar = SideBar(page)
+        self._active_view = HomePage(page)
         self.controls = [
             self.sidebar,
             self.toggle_nav_bar,
-            Text("Dupa"),
-            Text("Też dupa")
+            self.active_view
         ]
+        self.alignment = alignment.center
+        self.vertical_alignment = alignment.top_center
         self.expand= True
+    
+    @property
+    def active_view(self):
+        return self._active_view
+    
+    @active_view.setter
+    def active_view(self, view):
+        self._active_view = view
+        self.update()
         
     def toggle_nav_rail(self, e):
         self.sidebar.visible = not self.sidebar.visible
