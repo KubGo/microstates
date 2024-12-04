1782e7da1360ac15db8e896c0befe515b64a9a34
kubago <jakub.golcgdy@gmail.com>
2024-12-04 20:32:14 +0100

    Fix __init_ files

--- test_data_readers/__init__.py
+++ test_data_readers/__init__.py
@@ -1,4 +1,4 @@
-from data_identifiers import AbstractDataIdentifier, FilePathDataIdentifier, DelimiteredDataIdentifier
-from data_models import Data
-from data_read_strategies import CleanDataReadStrategy, DeleteStartEndDataStrategy
-from data_readers import FileDataReader, FolderDataReader
+from . data_identifiers import AbstractDataIdentifier, FilePathDataIdentifier, DelimiteredDataIdentifier
+from . data_models import Data
+from . data_read_strategies import AbstractDataReadStrategy, CleanDataReadStrategy, DeleteStartEndDataStrategy
+from . data_readers import FileDataReader, FolderDataReader, AbstractDataReader
